d58259fa92053ead23116933323505d7fb5f6392
Wittner Andrews <wittnerandrews@gmail.com>
2025-07-06 20:17:16 -0500

    Webpack and boilerplate setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './style.css';
+
+console.log('Webpack is working!');
+
+// Basic todo functionality will be added here
+function initTodoApp() {
+    console.log('Todo app initialized');
+}
+
+initTodoApp(); 
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Todo List',
+      template: './src/index.html',
+    }),
+  ],
+}; 
